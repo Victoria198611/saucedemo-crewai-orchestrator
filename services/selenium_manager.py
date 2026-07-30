c592419393075132b9d6c03025f235513ce85b13
Victoria Vutcariov <vutcariov5@mail.ru>
2026-07-30 17:23:03 +0300

    Configure Selenium Chrome for CI headless execution

--- services/selenium_manager.py
+++ services/selenium_manager.py
@@ -1,16 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 class SeleniumManager:
 
     _driver = None
 
-
     @classmethod
     def get_driver(cls):
 
         if cls._driver is None:
-            cls._driver = webdriver.Chrome()
+
+            options = Options()
+
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--window-size=1920,1080")
+
+            cls._driver = webdriver.Chrome(
+                options=options
+            )
 
         return cls._driver
 

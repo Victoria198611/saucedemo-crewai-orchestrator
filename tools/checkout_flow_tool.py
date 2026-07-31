75aa53ae64be1fac76c5ebbc579beedfbfc24779
Victoria Vutcariov <vutcariov5@mail.ru>
2026-07-31 12:08:29 +0300

    Fix BaseTool import and finalize CheckoutFlowTool

--- tools/checkout_flow_tool.py
+++ tools/checkout_flow_tool.py
@@ -1,3 +1,8 @@
+from crewai.tools import BaseTool
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from services.selenium_manager import SeleniumManager
 class CheckoutFlowTool(BaseTool):
 
     name: str = "checkout_flow"
